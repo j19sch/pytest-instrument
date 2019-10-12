f37decd9f651ed1ee585ade5bc4f9021b515ca3e
Joep Schuurkes <j19sch@gmail.com>
2019-10-12 19:46:22 +0200

    adds start, stop, duration to records

--- pytest_instrument/plugin.py
+++ pytest_instrument/plugin.py
@@ -14,15 +14,25 @@ def pytest_runtest_setup(item):
     item.user_properties.append(("instrument", instrument_marks))
 
 
+def pytest_runtest_makereport(item, call):
+    item.user_properties.append((call.when, {"start": call.start, "stop": call.stop}))
+
+
 def pytest_runtest_logreport(report):
     marks = ()
     for prop in (prop for prop in report.user_properties if prop[0] == "instrument"):
         marks = prop[1]
 
+    timestamps = {}
+    for prop in (prop for prop in report.user_properties if prop[0] == report.when):
+        timestamps = prop[1]
+
     record = {
         "node_id": report.nodeid,
         "when": report.when,
         "outcome": report.outcome,
+        "start": timestamps["start"],
+        "stop": timestamps["stop"],
         "duration": report.duration,
         "marks": marks,
     }
---
+++ tests/test_start_stop_duration.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+@pytest.fixture(scope="function")
+def tests_filename(testdir):
+    filename = "test_results_setup_teardown_call.py"
+    testdir.copy_example(filename)
+    return filename
+
+
+def test_record_start(testdir, tests_filename):
+    test_to_run = "test_passes"
+    result = testdir.runpytest("-vs", f"{tests_filename}::{test_to_run}")
+    result.assert_outcomes(error=0, failed=0, passed=1)
+
+    result.stdout.fnmatch_lines(f'---> record: *, "start": *')
+
+
+def test_record_stop(testdir, tests_filename):
+    test_to_run = "test_passes"
+    result = testdir.runpytest("-vs", f"{tests_filename}::{test_to_run}")
+    result.assert_outcomes(error=0, failed=0, passed=1)
+
+    result.stdout.fnmatch_lines(f'---> record: *, "stop": *')
+
+
+def test_record_duration(testdir, tests_filename):
+    test_to_run = "test_passes"
+    result = testdir.runpytest("-vs", f"{tests_filename}::{test_to_run}")
+    result.assert_outcomes(error=0, failed=0, passed=1)
+
+    result.stdout.fnmatch_lines(f'---> record: *, "duration": *')
