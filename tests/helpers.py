70392e883b930e073ffa1fea2329094d89a4be98
Joep Schuurkes <j19sch@gmail.com>
2019-10-21 10:59:40 +0200

    updates test_metadata to test csv file output only

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,19 @@
 import os
+import csv
+
+ARTIFACTS_DIRNAME = "artifacts"
+UUID4_REGEX = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}"
 
 
-def get_files_in_testdir_by_extension(dir_name, extension):
-    return [file for file in os.listdir(dir_name) if f".{extension}" in file]
+def get_files_from_artifacts_dir_by_extension(testdir, extension):
+    artifacts_dir = testdir.tmpdir.join(ARTIFACTS_DIRNAME)
+    return [file for file in os.listdir(artifacts_dir) if f".{extension}" in file]
+
+
+def get_records_from_csv_file_in_artifacts_dir(testdir, filename):
+    artifacts_dir = testdir.tmpdir.join(ARTIFACTS_DIRNAME)
+
+    with open(artifacts_dir.join(filename)) as csv_file:
+        csv_records = list(csv.DictReader(csv_file))
+
+    return csv_records
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,9 +1,7 @@
-import csv
 import pytest
-import re
 from uuid import UUID
 
-import tests.helpers
+from tests import helpers
 
 
 @pytest.fixture(scope="function")
@@ -20,27 +18,21 @@ def test_record_id(testdir, tests_filename):
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    uuid4_regex = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}"
-    result.stdout.re_match_lines(f'.*"record_id": "{uuid4_regex}",.*')
-
-    pattern = re.compile(f'.*"record_id": "({uuid4_regex})",.*')
-    record_ids = pattern.findall(result.stdout.str())
-
-    assert len(record_ids) == len(set(record_ids))
-
-    artifacts_dir = testdir.tmpdir.join("artifacts")
-    csv_files = tests.helpers.get_files_in_testdir_by_extension(artifacts_dir, "csv")
+    csv_files = helpers.get_files_from_artifacts_dir_by_extension(testdir, "csv")
     assert len(csv_files) == 1
 
-    csv_data = []
-    with open(artifacts_dir.join(csv_files[0])) as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for record in csv_reader:
-            try:
-                UUID(record["record_id"], version=4)
-            except (AttributeError, ValueError):
-                assert False, f"Record id {record['record_id']} is not a valid v4 UUID."
-            csv_data.append(dict(record))
+    csv_records = helpers.get_records_from_csv_file_in_artifacts_dir(
+        testdir, csv_files[0]
+    )
+
+    for record in csv_records:
+        try:
+            UUID(record["record_id"], version=4)
+        except (AttributeError, ValueError):
+            assert False, f"Record id {record['record_id']} is not a valid v4 UUID."
+
+    record_ids = [_["record_id"] for _ in csv_records]
+    assert len(record_ids) == len(set(record_ids))
 
 
 def test_session_id(testdir, tests_filename):
@@ -49,11 +41,18 @@ def test_session_id(testdir, tests_filename):
     )
     result.assert_outcomes(error=0, failed=0, passed=2)
 
-    uuid4_regex = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}"
-    result.stdout.re_match_lines(f'.*"session_id": "{uuid4_regex}",.*')
+    csv_files = helpers.get_files_from_artifacts_dir_by_extension(testdir, "csv")
+    assert len(csv_files) == 1
 
-    pattern = re.compile(f'.*"session_id": "({uuid4_regex})",.*')
-    session_id = pattern.search(result.stdout.str()).group(1)
+    csv_records = helpers.get_records_from_csv_file_in_artifacts_dir(
+        testdir, csv_files[0]
+    )
 
-    for line in [line for line in result.outlines if "---> record" in line]:
-        assert f'"session_id": "{session_id}"' in line
+    for record in csv_records:
+        try:
+            UUID(record["session_id"], version=4)
+        except (AttributeError, ValueError):
+            assert False, f"Session id {record['session_id']} is not a valid v4 UUID."
+
+    session_ids = [_["session_id"] for _ in csv_records]
+    assert len(set(session_ids)) == 1
