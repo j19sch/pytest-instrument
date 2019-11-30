af162e92040a2c48fddb6b95e98c0064ff50c94a
Joep Schuurkes <j19sch@gmail.com>
2019-11-30 09:55:24 +0100

    expands nodeid tests

--- tests/inner_tests/subdir_example/subdir/test_single_test_in_subdir_examples.py
+++ tests/inner_tests/subdir_example/subdir/test_single_test_in_subdir_examples.py
@@ -1,3 +1,3 @@
-def test_passes_in_subdir(request):
+def test_with_logger_passes_in_subdir(request):
     request.config.instrument["logger"].info("log record by test_passes in subdir")
     assert True
--- tests/test_node_id.py
+++ tests/test_node_id.py
@@ -10,7 +10,7 @@ def tests_filename(testdir):
     return filename
 
 
-def test_file_test(testdir, tests_filename):
+def test_with_test_as_function(testdir, tests_filename):
     test_to_run = "test_passes"
     result = testdir.runpytest(
         "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
@@ -26,7 +26,7 @@ def test_file_test(testdir, tests_filename):
     ) == len(records)
 
 
-def test_file_class_test(testdir, tests_filename):
+def test_with_test_in_class(testdir, tests_filename):
     class_to_run = "TestClass"
     test_to_run = "test_in_class_passes"
     result = testdir.runpytest(
@@ -43,11 +43,11 @@ def test_file_class_test(testdir, tests_filename):
     ) == len(records)
 
 
-def test_folder_file_test(testdir):
+def test_with_test_in_folder(testdir):
     example_folder = "subdir_example"
     tests_folder = "subdir"
     tests_filename = "test_single_test_in_subdir_examples.py"
-    test_to_run = "test_passes_in_subdir"
+    test_to_run = "test_with_logger_passes_in_subdir"
     testdir.copy_example(example_folder)
     result = testdir.runpytest(
         "-vs", "--instrument=json", f"{tests_folder}/{tests_filename}::{test_to_run}"
@@ -58,6 +58,22 @@ def test_folder_file_test(testdir):
     helpers.json_validate_each_record(records)
 
     expected_node_id = f"{tests_folder}/{tests_filename}::{test_to_run}"
+    assert len(
+        [record for record in records if record["node_id"] == expected_node_id]
+    ) == len(records)
+
+
+def test_with_test_using_all_fixtures_and_loggers(testdir, tests_filename):
+    test_to_run = "test_with_all_fixtures_and_logger"
+    result = testdir.runpytest(
+        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+    )
+    result.assert_outcomes(passed=1)
+
+    records = helpers.get_log_file_from_artifacts_dir_and_return_records(testdir)
+    helpers.json_validate_each_record(records)
+
+    expected_node_id = f"{tests_filename}::{test_to_run}"
     assert len(
         [record for record in records if record["node_id"] == expected_node_id]
     ) == len(records)
