c2e93bc7488796b10a455348d34bad0697593a4a
Joep Schuurkes <j19sch@gmail.com>
2019-11-30 18:25:25 +0100

    adds test for structlog sub-logger

--- tests/inner_tests/test_instr_logger_examples.py
+++ tests/inner_tests/test_instr_logger_examples.py
@@ -1,3 +1,12 @@
+import structlog
+
+
 def test_passes(instr_logger):
     instr_logger.error("Oh no, there is an error!")
-    pass
+
+
+def test_sub_logger(request):
+    logger = structlog.get_logger("instr.log.sublogger").bind(
+        session_id=request.config.instrument["session_id"], node_id=request.node.nodeid
+    )
+    logger.info("this actually works")
--- tests/log_records_tests/test_instr_logger.py
+++ tests/log_records_tests/test_instr_logger.py
@@ -27,11 +27,42 @@ def test_emit_log_record(testdir, tests_filename):
 
     record_name = "instr.log"
     record_level = "ERROR"
-    record_lineno = 2
+    record_lineno = 5
     record_message = "Oh no, there is an error!"
 
     result.stdout.fnmatch_lines(
         f"{record_level}    {record_name}:{tests_filename}:{record_lineno} {record_message}"
+    )
+    assert log_records[0]["message"] == record_message
+    assert log_records[0]["level"] == record_level.lower()
+    assert log_records[0]["lineno"] == record_lineno
+    assert log_records[0]["name"] == record_name
+
+
+def test_sublogger(testdir, tests_filename):
+    test_to_run = "test_sub_logger"
+    result = testdir.runpytest(
+        "-vs",
+        "--instrument=json",
+        "--log-cli-level=debug",
+        f"{tests_filename}::{test_to_run}",
+    )
+    result.assert_outcomes(error=0, failed=0, passed=1)
+
+    records = helpers.get_log_file_from_artifacts_dir_and_return_records(testdir)
+    log_records = [
+        record for record in records if record["name"] == "instr.log.sublogger"
+    ]
+    assert len(log_records) == 1
+    helpers.json_validate_each_record(records)
+
+    record_name = "instr.log.sublogger"
+    record_level = "INFO"
+    record_lineno = 12
+    record_message = "this actually works"
+
+    result.stdout.fnmatch_lines(
+        f"{record_level}     {record_name}:{tests_filename}:{record_lineno} {record_message}"
     )
     assert log_records[0]["message"] == record_message
     assert log_records[0]["level"] == record_level.lower()
