b6cb390152e8559c03e9bf48a0e877f8c95f5385
Joep Schuurkes <j19sch@gmail.com>
2019-12-15 10:53:47 +0100

    updates setup.py to v0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-instrument",
-    version="0.1.0",
+    version="0.2.0",
     author="Joep Schuurkes",
     author_email="j19sch@gmail.com",
     maintainer="Joep Schuurkes",
