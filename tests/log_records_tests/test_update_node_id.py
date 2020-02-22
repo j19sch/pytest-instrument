ba11915a7eb96058f66a8742199d0d3d8e34dbcd
Joep Schuurkes <j19sch@gmail.com>
2020-02-22 21:04:53 +0100

    completes updating tests for plain log file

--- tests/log_records_tests/test_update_node_id.py
+++ tests/log_records_tests/test_update_node_id.py
@@ -12,27 +12,42 @@ def tests_filename(testdir):
 
 def test_update_node_id(testdir, tests_filename):
     result = testdir.runpytest(
-        "-vs", "--instrument=json", "--log-cli-level=debug", f"{tests_filename}"
+        "-vs", "--instrument=json,log", "--log-cli-level=debug", f"{tests_filename}"
     )
     result.assert_outcomes(error=0, failed=0, passed=2)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    log_records = [
-        record for record in records if record["name"].startswith("instr.log")
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    json_records_instr_log = [
+        record for record in json_records if record["name"].startswith("instr.log")
     ]
-    assert len(log_records) == 4
-    helpers.json_validate_each_record(records)
+    assert len(json_records_instr_log) == 4
+    helpers.json_validate_each_record(json_records)
 
-    for record in log_records:
+    node_id_first_test = f"{tests_filename}::test_first_test"
+    node_id_second_test = f"{tests_filename}::test_second_test"
+
+    for record in json_records_instr_log:
         if record["message"] in ["fixture setup", "first test"]:
-            assert (
-                record["node_id"]
-                == "test_instr_logger_node_id_examples.py::test_first_test"
-            )
+            assert record["node_id"] == node_id_first_test
         elif record["message"] in ["second test", "fixture teardown"]:
-            assert (
-                record["node_id"]
-                == "test_instr_logger_node_id_examples.py::test_second_test"
-            )
+            assert record["node_id"] == node_id_second_test
+        else:
+            assert False, f"Unexpected message in record {record}"
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
+
+    log_records_instr_log = [record for record in log_records if "instr.log" in record]
+    assert len(log_records_instr_log) == len(json_records_instr_log)
+
+    for record in log_records_instr_log:
+        if "fixture setup" in record or "first test" in record:
+            assert node_id_first_test in record
+        elif "second test" in record or "fixture teardown" in record:
+            assert node_id_second_test in record
         else:
             assert False, f"Unexpected message in record {record}"
--- tests/report_record_tests/test_fixtures.py
+++ tests/report_record_tests/test_fixtures.py
@@ -14,17 +14,24 @@ def tests_filename(testdir):
 def test_setup_fixtures_with_different_scopes(testdir, tests_filename, fixture_scope):
     test_to_run = f"test_setup_fixture_{fixture_scope}_scope"
     result = testdir.runpytest(
-        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+        "-vs", "--instrument=json,log", f"{tests_filename}::{test_to_run}"
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    helpers.json_validate_each_record(records)
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    helpers.json_validate_each_record(json_records)
 
     expected_fixtures = [f"setup_fixture_with_{fixture_scope}_scope"]
     assert len(
-        [record for record in records if record["fixtures"] == expected_fixtures]
-    ) == len(records)
+        [record for record in json_records if record["fixtures"] == expected_fixtures]
+    ) == len(json_records)
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
 
 
 @pytest.mark.parametrize("fixture_scope", ["function", "module", "session"])
@@ -33,80 +40,115 @@ def test_teardown_fixtures_with_different_scopes(
 ):
     test_to_run = f"test_teardown_fixture_{fixture_scope}_scope"
     result = testdir.runpytest(
-        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+        "-vs", "--instrument=json,log", f"{tests_filename}::{test_to_run}"
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    helpers.json_validate_each_record(records)
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    helpers.json_validate_each_record(json_records)
 
     expected_fixtures = [f"teardown_fixture_with_{fixture_scope}_scope"]
     assert len(
-        [record for record in records if record["fixtures"] == expected_fixtures]
-    ) == len(records)
+        [record for record in json_records if record["fixtures"] == expected_fixtures]
+    ) == len(json_records)
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
 
 
 def test_multiple_fixtures(testdir, tests_filename):
     test_to_run = "test_with_multiple_fixtures"
     result = testdir.runpytest(
-        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+        "-vs", "--instrument=json,log", f"{tests_filename}::{test_to_run}"
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    helpers.json_validate_each_record(records)
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    helpers.json_validate_each_record(json_records)
 
     expected_fixtures = [
         "setup_fixture_with_function_scope",
         "teardown_fixture_with_function_scope",
     ]
     assert len(
-        [record for record in records if record["fixtures"] == expected_fixtures]
-    ) == len(records)
+        [record for record in json_records if record["fixtures"] == expected_fixtures]
+    ) == len(json_records)
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
 
 
 def test_without_fixtures(testdir, tests_filename):
     test_to_run = "test_with_no_fixtures"
     result = testdir.runpytest(
-        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+        "-vs", "--instrument=json,log", f"{tests_filename}::{test_to_run}"
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    helpers.json_validate_each_record(records)
-
-    assert len([record for record in records if record["fixtures"] is None]) == len(
-        records
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
     )
+    helpers.json_validate_each_record(json_records)
+
+    assert len(
+        [record for record in json_records if record["fixtures"] is None]
+    ) == len(json_records)
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
 
 
 def test_named_fixture(testdir, tests_filename):
     test_to_run = "test_named_fixture"
     result = testdir.runpytest(
-        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+        "-vs", "--instrument=json,log", f"{tests_filename}::{test_to_run}"
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    helpers.json_validate_each_record(records)
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    helpers.json_validate_each_record(json_records)
 
     expected_fixtures = ["named_fixture"]
     assert len(
-        [record for record in records if record["fixtures"] == expected_fixtures]
-    ) == len(records)
+        [record for record in json_records if record["fixtures"] == expected_fixtures]
+    ) == len(json_records)
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
 
 
 def test_child_fixture(testdir, tests_filename):
     test_to_run = "test_with_child_fixture"
     result = testdir.runpytest(
-        "-vs", "--instrument=json", f"{tests_filename}::{test_to_run}"
+        "-vs", "--instrument=json,log", f"{tests_filename}::{test_to_run}"
     )
     result.assert_outcomes(error=0, failed=0, passed=1)
 
-    records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(testdir)
-    helpers.json_validate_each_record(records)
+    json_records = helpers.get_json_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    helpers.json_validate_each_record(json_records)
 
     expected_fixtures = ["child_fixture", "parent_fixture"]
     assert len(
-        [record for record in records if record["fixtures"] == expected_fixtures]
-    ) == len(records)
+        [record for record in json_records if record["fixtures"] == expected_fixtures]
+    ) == len(json_records)
+
+    log_records = helpers.get_plain_log_file_from_artifacts_dir_and_return_records(
+        testdir
+    )
+    assert len(log_records[1:]) == len(json_records)
